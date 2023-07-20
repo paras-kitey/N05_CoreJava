aa1950315f5985147945090a9412ba18d82fac31
kitey <kitey@BEYONDIMAGINATION>
2023-07-20 12:04:46 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,42 @@
+// Program to demonstrate on user input
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner sc = new Scanner(System.in);
+ 
+ //System.out.println("Enter a character");
+ //char num2=sc.next().charAt(0);
+  //System.out.println("The Char is " + num2);
+	
+ 
+  System.out.println("Enter a String");
+  String str1=sc.nextLine();
+ // sc.next();
+  System.out.println("The String  is " + str1);
+  
+
+  
+  
+  
+  
+ 
+ 
+/* System.out.println("Enter a  float number");
+ float num1=sc.nextFloat();
+  System.out.println("The value of num is " + num1);
+	
+ 
+ 
+ 
+ System.out.println("Enter a number");
+ int num=sc.nextInt();
+  System.out.println("The value of num is " + num);*/
+	}
+
+}
