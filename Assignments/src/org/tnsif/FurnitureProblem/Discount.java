1df5f5cb128a1f5ba956fe2e3e43e66d6c522752
Paras <115292476+paras-kitey@users.noreply.github.com>
2023-10-01 20:03:39 +0530

    Delete Assignments/src/org/tnsif/FurnitureProblem directory

--- Assignments/src/org/tnsif/FurnitureProblem/Discount.java
+++
@@ -1,9 +0,0 @@
-/*
- * package org.tnsif.FurnitureProblem;
- * 
- * public class Discount { public double calculateDiscount( WoodenBero
- * woodenobj,SteelBero steelobj) { if(woodenobj != null ) { float totalprice = }
- * return 0;
- * 
- * } }
- */
